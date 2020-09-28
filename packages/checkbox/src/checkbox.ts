ba4ec0c9d8be2bfbd5a2f3a7993d96dd07506d57
Shashikant kalaskar <shashikant.kalaskar@coditas.com>
2020-09-28 10:20:34 +0530

    added accessibility

--- packages/checkbox/src/checkbox.ts
+++ packages/checkbox/src/checkbox.ts
@@ -23,6 +23,7 @@ export default class OrxeCheckbox extends TranslationClass {
     isDisabled: false,
     state: 'active',
     isRequired: false,
+    labelId: 'label_1',
     key: 'key_2',
     metaData: {
 
@@ -39,6 +40,7 @@ export default class OrxeCheckbox extends TranslationClass {
         isDisabled: false,
         state: 'error',
         key: 'key_3',
+        labelId: 'label_2',
         metaData: {
           value: 550
         },
@@ -49,6 +51,7 @@ export default class OrxeCheckbox extends TranslationClass {
         isDisabled: false,
         state: 'error',
         key: 'key_4',
+        labelId: 'label_3',
         metaData: {
           value: 550
         },
@@ -77,47 +80,72 @@ export default class OrxeCheckbox extends TranslationClass {
       let checkedCheckBox = this.checkBoxGroup.checkBoxItems.find(check => {
         return check.isChecked;
       });
-      console.log(checkedCheckBox);
       if (checkedCheckBox) {
         this.errorMessage = '';
       } else {
         this.errorMessage = 'Please check one of the checkbox';
       }
+      const event = new CustomEvent('checkBoxGroupEvent', {
+        detail: {
+          checkBoxGroup: this.checkBoxGroup
+        },
+        // bubbles: false
+      });
+      this.dispatchEvent(event);
     }
   }
 
   public getCheckbox() {
     let template;
     if (this.checkboxType === 'checkbox') {
-      template = this.getTemplateForCheckBox(this.checkBoxItem)
+      template = this.getTemplateForCheckBox(this.checkBoxItem, 0)
     } else if (this.checkboxType === 'checkboxGroup') {
-      return this.checkBoxGroup.checkBoxItems.map(checkbox => {
-        return this.getTemplateForCheckBox(checkbox);
+      return this.checkBoxGroup.checkBoxItems.map((checkbox, i) => {
+        return this.getTemplateForCheckBox(checkbox, i);
       });
     }
     return template;
   }
 
-  public getTemplateForCheckBox(checkbox) {
+  public getTemplateForCheckBox(checkbox, index) {
     return html`
             <div class="checkbox-container">
             <div>
-            <label class="main">
-                <input data-testid="container" type="checkbox" id="${checkbox.key}" @change="${() => { this.checkUncheckBox(checkbox) }}" ?disabled=${checkbox.isDisabled} ?checked=${checkbox.isChecked}>
-                <span for="${checkbox.key}" class="checkbox ${this.getClassByState(checkbox)} ${this.getClass()}"></span>
-                <span class="checkbox-label-name">${checkbox.label}</span>
-            </label>
+            <div class="main" @click="${(event) => { this.checkUncheckBox(event, checkbox) }}">
+            <label class="checkbox-label-name" aria-label="${checkbox.label}" for="${checkbox.key}" id="${checkbox.labelId}">${checkbox.label}</label>
+            <span class="checkbox" id="${checkbox.key}" tabindex="${index}" role="checkbox"
+            @keypress="${(event) => { this.onKeyPress(event, checkbox) }}" aria-checked="${checkbox.isChecked}" aria-disabled=${checkbox.isDisabled} 
+             aria-labelledby="${checkbox.labelId}">
+            <span class="checked-mark"></span>
+            </span>
+            </div>
             </div>
             ${this.getMetaData(checkbox)}
             </div>`
+
+    // <input data-testid="container" type="checkbox" id="${checkbox.key}" @change="${() => { this.checkUncheckBox(checkbox) }}" ?disabled=${checkbox.isDisabled} ?checked=${checkbox.isChecked}>
+    //     <span for="${checkbox.key}" class="checkbox ${this.getClassByState(checkbox)} ${this.getClass()}"></span>
+    //     <span class="checkbox-label-name">${checkbox.label}</span>
+  }
+
+  public onKeyPress(event, checkbox) {
+    let isEnterOrSpace = event.keyCode === 32 || event.keyCode === 13;
+    if (isEnterOrSpace) {
+      this.checkUncheckBox(event, checkbox);
+    }
   }
 
   public getClass() {
     return this.errorMessage ? 'error-message-border' : ''
   }
 
-  public checkUncheckBox(checkbox) {
-    if (this.checkboxType == 'checkboxGroup') {
+  public checkUncheckBox(event, checkbox) {
+    if (!checkbox.isDisabled) {
+      console.log(event.srcElement.ariaChecked);
+      event.srcElement.classList.toggle('is-checked', !checkbox.isChecked);
+      event.srcElement.setAttribute('aria-checked', !checkbox.isChecked);
+    }
+    if (this.checkboxType == 'checkboxGroup' && !checkbox.isDisabled) {
       this.checkBoxGroup.checkBoxItems.map(check => {
         if (checkbox.key === check.key) {
           check.isChecked = !check.isChecked;
@@ -125,13 +153,20 @@ export default class OrxeCheckbox extends TranslationClass {
         }
       });
       this.checkErrorMessage();
-    } else {
+    } else if (!checkbox.isDisabled) {
       checkbox.isChecked = !checkbox.isChecked;
-      if (!checkbox.isChecked && checkbox.isRequired) { 
+      if (!checkbox.isChecked && checkbox.isRequired) {
         this.errorMessage = 'You will have to select the checkbox';
-      }else{
-        this.errorMessage ='';
+      } else {
+        this.errorMessage = '';
       }
+      const event = new CustomEvent('checkBoxItemEvent', {
+        detail: {
+          checkBoxItem: this.checkBoxItem
+        },
+        // bubbles: false
+      });
+      this.dispatchEvent(event);
     }
   }
 
